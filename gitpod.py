bc5b42f1d674081eca145f90809fe1bb9825383e
MiyanMei0001 <miyanmei0001@gmail.com>
2024-12-24 00:21:14 +0700

    Update

--- gitpod.py
+++ gitpod.py
@@ -4,7 +4,7 @@ import json
 url = "https://gitpod.io/public-api/gitpod.v1.WorkspaceService/StartWorkspace"
 
 payload = {
-  "workspaceId": "miyan0001-miyan-90008ynfu79"
+  "workspaceId": "miyanmei0001-miyanmei-yebr8ss55pa"
 }
 
 headers = {
@@ -22,7 +22,7 @@ headers = {
   'referer': "https://gitpod.io/start/",
   'accept-language': "en,en-US;q=0.9",
   'priority': "u=1, i",
-  'Cookie': "GCLB=COa_57b-6Y6N_QEQAw; gp-necessary=true; gp-analytical=true; gitpod-marketing-website-visited=true; gp-targeting=true; ajs_anonymous_id=a7c78c3a-28e1-47fd-b422-7381ae34008f; skip-signup-wall=true; _gcl_au=1.1.1341546114.1730610395; _ga=GA1.1.390662381.1730610397; gitpod_hashed_user_id=3c6fc21592609cbaac5e7d605ff9415f; _ga_VWLSB1WXM5=GS1.1.1732587675.3.0.1732587675.0.0.0; __Host-_gitpod_io_jwt2_=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjAwMDEifQ.eyJpYXQiOjE3MzQ4NzA0NDEsImV4cCI6MTczNTQ3NTI0MSwiaXNzIjoiaHR0cHM6Ly9naXRwb2QuaW8iLCJzdWIiOiI1YjVkYzk1OS0yMmNjLTRhMWQtODM5NC0yZjkwNGZhZmYwNGUifQ.Y6h1DMhpFF89XkbLn2sdw-lYV7bcayqk5ptO7IkxE6oeow_WraVck5WcWKzGP7lKaSM9RwW3hWCBmpPDcS728bJ9hGYJSWDmFNZ9YI7tzRNGOg_Wo3yPXtlUkmvlqnHErA_WXKoQ6QOd1FpzIoMpNocIeCccbGJ5h7K0nEEbpEGVgnY9HPZTG3J9xoEX_Z_wVUmuQBGZ5qn2d6TcRawc9bmKuUsiSf6IQQG_KPvDpAyaqnfVlAHHLDFiGGihE2LZVYsxq52974yd1CNW7yR8cgtEnL8pdF17oVVvysTESWW31gDpZCc4k3Z8M-HdFa_MqxxCZUug2NVGLwZ0rNFJ3U8ib5j4AVEZVF-eQiYV8LC4s15u3Q6g2Ocfgy3uGPKB_YzyE5b9GpoD1Tt5eEuj7ceGhmtMmAshmQ5b9_mBgSNttxm29NLSB_qnTQ9HWDu-J5ey7PtdsT4xXxj_dMIgma-tIHvGokBALjsLt5rfUsG_iEaO2tyxVMlfkld4WlOyjodEWfmAZCzd7maD7BZSLZU9wVsqr01d9QCYNf5nFGinU8KehFA6wzEL2BcAm5DDV0IevcIWBOAWFKqt6OCbCu7TNF54ttwPrpWxYFqLkWsBcF_OBR-o0tZI4-LklsQDW5jaaBngHcbVdtHrbUbICoEjG_AhVNGh5J7Cv8_OT-0"
+  'Cookie': "GCLB=COa_57b-6Y6N_QEQAw; gp-necessary=true; gp-analytical=true; gitpod-marketing-website-visited=true; gp-targeting=true; ajs_anonymous_id=a7c78c3a-28e1-47fd-b422-7381ae34008f; skip-signup-wall=true; _gcl_au=1.1.1341546114.1730610395; _ga=GA1.1.390662381.1730610397; gitpod_hashed_user_id=3c6fc21592609cbaac5e7d605ff9415f; _ga_VWLSB1WXM5=GS1.1.1732587675.3.0.1732587675.0.0.0; __Host-_gitpod_io_jwt2_=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjAwMDEifQ.eyJpYXQiOjE3MzQ5NzQzMTIsImV4cCI6MTczNTU3OTExMiwiaXNzIjoiaHR0cHM6Ly9naXRwb2QuaW8iLCJzdWIiOiI1YjVkYzk1OS0yMmNjLTRhMWQtODM5NC0yZjkwNGZhZmYwNGUifQ.PqRIKrfD5UkWs3njInl3_QrxCZohQIisrtlLLgrOqKx0D58ghQWLphz9Js7vrvbEjAZlLbsC7eZTrvAnrzmILhaRtLYM5s687noPKnK9LKihTr1JDsZZ_icwY5fASp-7auwIUVy3H6He1SgbUEOotJtLmrHS6wAp2VTmouLrAMY2zff0437I2KFtYZ8wr8Z58cpLWAR7_auU8Z4htyclaDQGngeEvGY-3JUXE8ttX-3u4lkfTWZaDqi4kACMG9aMEq6Q7rBV5oEu-o3VZPFr6B_PXmHG7d32gEIw6UEVpN8sbpH6DW1qXt3K8vbgDxHK0gEvM1bW3-etUiNfvtv3hIZFC-C2rfEbwjLtBjGuu7Rq1TYTXWILs3OSRcuSn8aiQ6ZDFTDvHw67TknxpNSNmuzPST9-JLL8163_JlJ2GUDRNkiptrHJBihDZqxqRdrUyfF3y3YczroX5zlunmNer0Vod79SZ9aTepX7lfZsKR4krFL60-gyQHqEeqOxbRTbaCt4Zk0drlS7BJreZeyaalIKBAezfM6JfgihY2t1C3yBksZE0InrOK3cH3eT5PMDO4qHDxf271RAHdA2ubikTcOu5NXCPHP9s0--rK_Ii_rVGHs1F2lo10LMwGoyELHl0bDaXskmdyUDMDoebQnSKyQh8rhfQ4EZEu4oiXG8QfE"
 }
 
 response = requests.post(url, data=json.dumps(payload), headers=headers)
